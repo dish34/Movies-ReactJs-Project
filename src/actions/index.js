2cbe63e121773c3032427360f0580d92f733fe8c
dishant <dishantgandhi99@yahoo.com>
2020-09-06 23:59:00 +0530

    Changing the State

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,3 +2,14 @@
 //     type:'ADD_MOVIES',
 //     movies: [],
 // }
+
+// action Types
+export const ADD_MOVIES = "ADD_MOVIES";
+
+//action creators
+export function addMovies(movies) {
+  return {
+    type: ADD_MOVIES,
+    movies: movies,
+  };
+}
--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { data } from "../data";
 import Navbar from "./Navbar";
 import MovieCard from "./MovieCard";
+import { addMovies } from "../actions";
 
 class App extends React.Component {
   componentDidMount() {
@@ -12,14 +13,11 @@ class App extends React.Component {
     });
     // make api calls
     // dispatch actions
-    store.dispatch({
-      type: "ADD_MOVIES",
-      movies: data,
-    });
+    store.dispatch(addMovies(data));
     console.log("HI", store.getState());
   }
   render() {
-    const movies = this.props.store.getState();
+    const { list } = this.props.store.getState();
     return (
       <div className="App">
         <Navbar />
@@ -29,7 +27,7 @@ class App extends React.Component {
             <div className="tab">Favourites</div>
           </div>
           <div className="list">
-            {movies.map((movie, index) => (
+            {list.map((movie, index) => (
               <MovieCard movie={movie} key={`movies-${index}`} />
             ))}
           </div>
